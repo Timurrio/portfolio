12c56b385c98c2030c4d82c8620d47742593e268
Tymur Myronenko <50523488+Timurrio@users.noreply.github.com>
2024-02-01 17:43:06 +0200

    Created ScrollColorText component
    Moved some styles and functionality from FooterSection to new component

--- src/components/FooterSection/FooterSection.tsx
+++ src/components/FooterSection/FooterSection.tsx
@@ -1,27 +1,17 @@
+import { useRef } from "react"
+import { ScrollColorText } from "../ScrollColorText/ScrollColorText"
 import styles from "./FooterSection.module.scss"
 import { motion } from "framer-motion"
 
 export const FooterSection = () => {
+    const scrollContainerRef = useRef(null)
+
     return (
         <div className={styles.container}>
-            <div className={styles.header}>
-                <span className={styles.span}>
-                    <motion.span transition={{ duration: 1 }} initial={{ clipPath: "inset(100% 0% 0% 0%)" }} animate={{ clipPath: "inset(0% 0% 0% 0%)" }} className={styles["span-top"]}>T</motion.span>
-                    {/* <span className={styles["span-bottom"]}>T</span> */}
-                    <span className={styles["span-bottom"]}>T</span>
-                </span>
-                <span className={styles.span}>
-                    <span className={styles["span-bottom"]}>E</span>
-                    <span className={styles["span-top"]}>E</span>
-                </span>
-                <span className={styles.span}>
-                    <span className={styles["span-bottom"]}>X</span>
-                    <span className={styles["span-top"]}>X</span>
-                </span>
-                <span className={styles.span}>
-                    <span className={styles["span-bottom"]}>T</span>
-                    <span className={styles["span-top"]}>T</span>
-                </span>
+            <div ref={scrollContainerRef} className={styles["scroll-container"]}>
+
+                <ScrollColorText colors={["#FFF5EE", "#DC143C"]} target={scrollContainerRef} text="TEXT" />
+
             </div>
         </div>
     )
---
+++ src/components/ScrollColorText/ScrollColorText.tsx
@@ -0,0 +1,36 @@
+import { RefObject } from "react"
+import styles from "./ScrollColorText.module.scss"
+import { motion, useScroll, useTransform, useSpring } from "framer-motion"
+
+interface ScrollColorTextProps {
+    target: RefObject<HTMLElement>,
+    text: string,
+    colors?: string[],
+    scrollOffset?: string[]
+}
+
+export const ScrollColorText = ({ target, text, colors = ["#2657eb", "#2657eb"], scrollOffset = ["start start", "start end"] }: ScrollColorTextProps) => {
+    // @ts-ignore
+    const { scrollYProgress } = useScroll({ target: target, offset: scrollOffset })
+    const textFillProgress = useTransform(useSpring(scrollYProgress), [0, 1], ["inset(0% 0% 0% 0%)", "inset(100% 0% 0% 0%)"])
+    const colorsTransform = useTransform(scrollYProgress, [0, 1], [colors[1], colors[0]])
+    return (
+        <div className={styles.container}>
+            {
+                text.split("").map((letter) => (
+                    <span className={styles.span}>
+                        <span aria-hidden className={styles["span-bottom"]}>{letter}</span>
+                        <motion.span
+                            aria-hidden
+                            style={{ clipPath: textFillProgress, color: colorsTransform }}
+                            className={styles["span-top"]}>
+
+                            {letter}
+                        </motion.span>
+                        {letter}
+                    </span>
+                ))
+            }
+        </div>
+    )
+}
