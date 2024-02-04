665147a07dbeecf68b9d7270ae6713a19ec67d29
Tymur Myronenko <50523488+Timurrio@users.noreply.github.com>
2024-02-04 14:03:54 +0200

    Work-in-Progress: Enhanced FooterSection Content and Styling

--- src/components/FooterSection/FooterSection.tsx
+++ src/components/FooterSection/FooterSection.tsx
@@ -2,17 +2,39 @@ import { useRef } from "react"
 import { ScrollColorText } from "../ScrollColorText/ScrollColorText"
 import styles from "./FooterSection.module.scss"
 import { motion } from "framer-motion"
+import { GithubIcon } from "../Icons/GithubIcon"
+import { LinkedinIcon } from "../Icons/LinkedinIcon"
 
 export const FooterSection = () => {
     const scrollContainerRef = useRef(null)
 
     return (
-        <div className={styles.container}>
+        <footer className={styles.container}>
             <div ref={scrollContainerRef} className={styles["scroll-container"]}>
+                <div className={styles["footer-header"]}>
+                    <ScrollColorText colors={["white", "white"]} target={scrollContainerRef} text="LET`S" />
+                    <ScrollColorText colors={["white", "white"]} target={scrollContainerRef} text="WORK" />
+                    <ScrollColorText colors={["#de6161", "#2657eb"]} target={scrollContainerRef} text="TOGETHER!" />
+                </div>
 
-                <ScrollColorText colors={["#FFF5EE", "#DC143C"]} target={scrollContainerRef} text="TEXT" />
+                <div className={styles["mail-button-wrapper"]}>
+                    <a href="mailto:tymur.myronenko@gmail.com" className={styles["mail-button-link"]}>
+                        <div className={styles["mail-button"]}>
+                            tymur.myronenko@gmail.com
+                        </div>
+                    </a>
+                </div>
+
+                <div className={styles["copyright-section"]}>
+                    <p className={styles["copyright-text"]}>© Myronenko Tymur. All rights are reserved 2024.</p>
+                    <div className={styles["copyright-icons"]}>
+                        <GithubIcon />
+                        <LinkedinIcon />
+                    </div>
+
+                </div>
 
             </div>
-        </div>
+        </footer>
     )
 }
