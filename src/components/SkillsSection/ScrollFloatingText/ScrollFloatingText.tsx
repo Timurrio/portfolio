98bec5c26f3b9e1f11d3c3cd189b13e3bb8a719a
Tymur Myronenko <50523488+Timurrio@users.noreply.github.com>
2024-02-20 19:11:54 +0200

    Added info in SkillsSection and updated styles

--- src/components/SkillsSection/ScrollFloatingText/ScrollFloatingText.tsx
+++ src/components/SkillsSection/ScrollFloatingText/ScrollFloatingText.tsx
@@ -9,29 +9,28 @@ import { motion, useScroll, useTransform, useMotionValueEvent, useSpring } from 
 export const ScrollFloatingText = () => {
     const containerRef = useRef(null)
     const textRef = useRef(null)
-    const { scrollYProgress } = useScroll({ target: containerRef, offset: ["start center", "end end"] })
+    const { scrollYProgress } = useScroll({ target: textRef, offset: ["start end", "end start"] })
     const currentY = useTransform(useSpring(scrollYProgress), [0, 1], ['0vh', '-100vh'])
-    const opacity = useTransform(scrollYProgress, [0, 0.8, 1], [0.4, 1, 1])
     const rotation1 = useTransform(useSpring(scrollYProgress), [0, 1], ['-13deg', '13deg'])
-    const rotation2 = useTransform(useSpring(scrollYProgress), [0, 1], ['13deg', '-6deg'])
-    const rotation3 = useTransform(useSpring(scrollYProgress), [0, 1], ['10deg', '-7deg'])
-    const rotation4 = useTransform(useSpring(scrollYProgress), [0, 1], ['-8deg', '10deg'])
+    const rotation2 = useTransform(useSpring(scrollYProgress), [0, 1], ['8deg', '-18deg'])
+    const rotation3 = useTransform(useSpring(scrollYProgress), [0, 1], ['15deg', '-13deg'])
+    const rotation4 = useTransform(useSpring(scrollYProgress), [0, 1], ['-8deg', '18deg'])
 
 
     return (
         <motion.div className={styles.container} ref={containerRef} >
 
-            <motion.div className={styles["text-wrapper"]} style={{ opacity, y: currentY, rotate: rotation2, left: "8%" }}>
-                <ScrollColorText colors={["#CF649B", "#CF649B"]} text="SCSS" target={textRef} scrollOffset={["start end", "start start"]} />
+            <motion.div className={styles["text-wrapper"]} style={{ y: currentY, rotate: rotation2, left: "8%" }}>
+                <ScrollColorText colors={["#CF649B", "#CF649B"]} text="SCSS" target={textRef} scrollOffset={["start end", "start 20%"]} />
             </motion.div>
-            <motion.div ref={textRef} className={styles["text-wrapper"]} style={{ opacity, y: currentY, rotate: rotation1, bottom: "17%" }}>
-                <ScrollColorText text="TYPESCRIPT" target={textRef} scrollOffset={["start end", "start start"]} />
+            <motion.div ref={textRef} className={styles["text-wrapper"]} style={{ y: currentY, rotate: rotation1, bottom: "17%" }}>
+                <ScrollColorText text="TYPESCRIPT" target={textRef} scrollOffset={["start end", "start 20%"]} />
             </motion.div>
-            <motion.div className={styles["text-wrapper"]} style={{ opacity, y: currentY, rotate: rotation3, right: "5%", top: "5%" }}>
-                <ScrollColorText colors={["#E44D25", "#E44D25"]} text="HTML" target={textRef} scrollOffset={["start end", "start start"]} />
+            <motion.div className={styles["text-wrapper"]} style={{ y: currentY, rotate: rotation3, right: "5%", top: "5%" }}>
+                <ScrollColorText colors={["#E44D25", "#E44D25"]} text="HTML" target={textRef} scrollOffset={["start end", "start 20%"]} />
             </motion.div>
-            <motion.div className={styles["text-wrapper"]} style={{ opacity, y: currentY, rotate: rotation4, bottom: "10%", right: "8%" }}>
-                <ScrollColorText colors={["#61DBFB", "#61DBFB"]} text="REACT" target={textRef} scrollOffset={["start end", "start start"]} />
+            <motion.div className={styles["text-wrapper"]} style={{ y: currentY, rotate: rotation4, bottom: "10%", right: "8%" }}>
+                <ScrollColorText colors={["#61DBFB", "#61DBFB"]} text="REACT" target={textRef} scrollOffset={["start end", "start 20%"]} />
             </motion.div>
 
         </motion.div>
--- src/components/SkillsSection/SkillsSection.tsx
+++ src/components/SkillsSection/SkillsSection.tsx
@@ -1,10 +1,43 @@
 import { ScrollFloatingText } from "./ScrollFloatingText/ScrollFloatingText"
 import styles from "./SkillsSection.module.scss"
+import { motion } from "framer-motion"
+
+const stackNames = ["HTML", "CSS (SCSS)", "Javascript (Typescript)", "React", "Redux", "Framer Motion (animation)"]
+const infoVariants = {
+    hidden: {
+        opacity: 0,
+    },
+    visible: {
+        opacity: 1,
+    }
+}
+const techVariants = {
+    hidden: {
+        opacity: 0,
+        x: -100
+    },
+    visible: {
+        opacity: 1,
+        x: 0
+    }
+}
 
 export const SkillsSection = () => {
 
     return (
         <div className={styles.container}>
+            <motion.div className={styles.info} variants={infoVariants} transition={{ staggerChildren: 0.2, duration: 1 }} initial={"hidden"} whileInView={"visible"} viewport={{ once: true, amount: 1 }}>
+                <h2 className={styles.header}>My Tech Stack 👨‍💻</h2>
+                <motion.ul className={styles.stack}>
+                    {
+                        stackNames.map((str) => (
+                            <motion.li variants={techVariants} transition={{ duration: 0.7, type: "spring", stiffness: 150, mass: 1.2 }}>
+                                {str}
+                            </motion.li>
+                        ))
+                    }
+                </motion.ul>
+            </motion.div>
             <ScrollFloatingText />
         </div>
     )
